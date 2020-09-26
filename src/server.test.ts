da6437722c27b14f04c05ffa864f54d43efb6b13
luizzbanndera <31023594+LuizzBanndera@users.noreply.github.com>
2020-09-26 12:28:55 -0300

    updated: login test

--- src/server.test.ts
+++
@@ -1,8 +0,0 @@
-import req from 'supertest'
-import app from './app'
-
-test('base', async () => {
-  await req(app).get('/', (res) => {
-    expect(res.text).toBe('api working!!!')
-  })
-}, 30000)
--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -1,11 +1,23 @@
 import req from 'supertest'
 import app from '../app'
+import auth from '../middlewares/authentication'
+
+test('auth', async () => {
+
+  jest.mock('../middlewares/authentication', () => jest.fn((req, res, next) => next()))
+
+  await req(app).get('/users', (res) => {
+    expect(res.code).toBe(200)
+    expect(auth).toHaveBeenCalledTimes(1)
+  })
+}, 30000)
 
 test('[POST] /users/login', async () => {
   await req(app).put('/users/login', (res) => {
     expect(res.code).toBe(200)
   })
 }, 30000)
+
 
 test('[GET] /users', async () => {
   await req(app).get('/users', (res) => {
